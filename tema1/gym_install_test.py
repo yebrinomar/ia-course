a74ebbcf6162c6301a5f149cc833095c56d9ddf7
Omar <yebrinomar@gmail.com>
2021-01-04 23:46:16 -0300

    Update run mountain

--- tema1/gym_install_test.py
+++ tema1/gym_install_test.py
@@ -4,10 +4,10 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
 import gym # cargamos la librería de OpenAI Gym
 
-environment = gym.make("MountainCar-v0") # Lanzamos una instancia del videojuego de la Montaña rusa
+#'SpaceInvaders-v0'
+environment = gym.make('MountainCar-v0') # Lanzamos una instancia del videojuego de la Montaña rusa
 environment.reset() # Limpiamos y preparamos el entorno para tomar decisiones
 for _ in range(2000): # Durante 2000 iteraciones (veces)
     environment.render() # Pintamos en pantalla la acción
